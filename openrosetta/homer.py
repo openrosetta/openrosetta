a2c89b1807317dfb62cb2ca665020ab519680b50
Simone Marzola <marzolasimone@gmail.com>
2014-05-17 22:04:03 +0200

    homer proxy

--- openrosetta/homer.py
+++ openrosetta/homer.py
@@ -1,1 +1,11 @@
-__author__ = 'simock85'
+import requests
+
+
+class HomerAdapter(object):
+    base_url = 'http://opendata-federation.csi.it/fed-homer/documents/select/'
+
+    def proxy(self, **query):
+        r = requests.get(self.base_url, **query)
+        return r.json()
+
+homer_adapter = HomerAdapter()
--- openrosetta/views.py
+++ openrosetta/views.py
@@ -1,6 +1,16 @@
 """ Cornice services.
 """
+from colander import MappingSchema, SchemaNode, String, OneOf, Integer
 from cornice import Service
+from openrosetta.homer import homer_adapter
+
+
+class HomerSchema(MappingSchema):
+    q = SchemaNode(String(), location='querystring')
+    lang = SchemaNode(String(), location='querystring')
+    wt = SchemaNode(String(), location='querystring', missing='json', validator=OneOf(['json']))
+    start = SchemaNode(Integer(), location='querystring')
+    rows = SchemaNode(Integer(), location='querystring')
 
 
 hello = Service(name='hello', path='/', description="Simplest app")
@@ -14,6 +24,7 @@ def get_info(request):
 
 homer = Service(name='homer', path='/homer', description='Homer endpoint proxy')
 
-@homer.get()
-def homer(request):
-    pass
+
+@homer.get(schema=HomerSchema)
+def get_homer(request):
+    return homer_adapter.proxy(params=request.validated)
