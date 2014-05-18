05748026311d4e9bbeae65ec1fde74826c34e569
Marcello Cardea <marcellocardea@MacBook-Pro-di-Marcello.local>
2014-05-18 08:07:11 +0200

    added pick your physician

---
+++ pickyourphysician/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,10 @@
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes.dex : \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes/com/ionicframework/starter/BuildConfig.class \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes/com/ionicframework/starter/HelloCordova.class \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes/com/ionicframework/starter/R$attr.class \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes/com/ionicframework/starter/R$drawable.class \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes/com/ionicframework/starter/R$string.class \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes/com/ionicframework/starter/R$xml.class \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes/com/ionicframework/starter/R.class \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/classes/org/apache/cordova/device/Device.class \
+/Users/marcellocardea/Hack4Med/pickyourphysician/platforms/android/ant-build/dexedLibs/classes-6b937ff26e79c6fe3eb5b7ba0a2f5757.jar \
